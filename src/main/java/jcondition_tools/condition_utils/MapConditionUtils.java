6cc5755704551b4493862ea2ae63de24843faec6
SeanDorff <seandorff@googlemail.com>
2023-03-04 13:48:47 +0100

    - Added containsTuple method

--- src/main/java/jcondition_tools/condition_utils/MapConditionUtils.java
+++ src/main/java/jcondition_tools/condition_utils/MapConditionUtils.java
@@ -17,6 +17,36 @@ public class MapConditionUtils {
     }
 
     /**
+     * @param map   to be checked
+     * @param entry to be compared
+     * @return {@code true} if {@code map} is not {@code null} or empty and contains the tuple {@code entry}
+     */
+    public static <K, V> boolean containsTuple(Map<K, V> map, Map.Entry<K, V> entry) {
+        if (isNullOrEmpty(map)) {
+            return false;
+        }
+        if (entry == null) {
+            return false;
+        }
+
+        K entryKey = entry.getKey();
+        if (map.containsKey(entryKey)) {
+            V mapValue = map.get(entryKey);
+            V entryValue = entry.getValue();
+            if (ObjectConditionUtils.areNull(entryValue, mapValue)) {
+                return true;
+            }
+            if (entryValue == null) {
+                return false;
+            }
+            if (entryValue.equals(mapValue)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
      * @param map to be checked
      * @return {@code true} if {@code map} is not {@code null} or empty
      */
--- src/test/java/jcondition_tools/condition_utils/MapConditionUtilsTest.java
+++ src/test/java/jcondition_tools/condition_utils/MapConditionUtilsTest.java
@@ -3,6 +3,7 @@ package jcondition_tools.condition_utils;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -12,6 +13,85 @@ class MapConditionUtilsTest {
     static final Map<Object, Object> EMPTY_MAP = Collections.emptyMap();
     static final Map<Character, Character> NON_EMPTY_MAP = Map.of('a', 'b');
     static final Map NULL_MAP = null;
+    static final Map.Entry<Character, Character> TUPLE_A_B = new Map.Entry<>() {
+        private Character value = 'b';
+
+        @Override
+        public Character getKey() {
+            return 'a';
+        }
+
+        @Override
+        public Character getValue() {
+            return value;
+        }
+
+        @Override
+        public Character setValue(Character value) {
+            this.value = value;
+            return value;
+        }
+    };
+    static final Map.Entry<Character, Character> TUPLE_C_D = new Map.Entry<>() {
+        private Character value = 'd';
+
+        @Override
+        public Character getKey() {
+            return 'c';
+        }
+
+        @Override
+        public Character getValue() {
+            return value;
+        }
+
+        @Override
+        public Character setValue(Character value) {
+            this.value = value;
+            return value;
+        }
+    };
+    static final Map.Entry<Character, Character> TUPLE_E_NULL = new Map.Entry<>() {
+        private Character value = null;
+
+        @Override
+        public Character getKey() {
+            return 'e';
+        }
+
+        @Override
+        public Character getValue() {
+            return value;
+        }
+
+        @Override
+        public Character setValue(Character value) {
+            this.value = value;
+            return value;
+        }
+    };
+    static final Map<Character, Character> NULL_VALUE_MAP = generateNullValueMap();
+
+    private static Map<Character, Character> generateNullValueMap() {
+        Map<Character, Character> nullValueMap = new HashMap<>();
+        nullValueMap.put('e', null);
+        return nullValueMap;
+    }
+
+    @Test
+    void testContainsTupleNegative() {
+        assertFalse(MapConditionUtils.containsTuple(NULL_MAP, null));
+        assertFalse(MapConditionUtils.containsTuple(EMPTY_MAP, null));
+        assertFalse(MapConditionUtils.containsTuple(NULL_MAP, TUPLE_A_B));
+        assertFalse(MapConditionUtils.containsTuple(NON_EMPTY_MAP, null));
+        assertFalse(MapConditionUtils.containsTuple(NON_EMPTY_MAP, TUPLE_C_D));
+    }
+
+    @Test
+    void testContainsTuplePositive() {
+        assertTrue(MapConditionUtils.containsTuple(NON_EMPTY_MAP, TUPLE_A_B));
+        assertTrue(MapConditionUtils.containsTuple(NULL_VALUE_MAP, TUPLE_E_NULL));
+    }
 
     @Test
     void testIsNotNullOrEmptyNegative() {
